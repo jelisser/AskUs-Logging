a14b7da9c0c25b625c127aad70b628a0e77d12ed
Juli Lisser <jelisser@uwm.edu>
2018-11-19 15:45:34 -0600

    login

--- chat.py
+++ chat.py
@@ -12,10 +12,11 @@ import logging
 import redis
 import gevent
 import datetime
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,flash,session,redirect,abort
 from flask_sockets import Sockets
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
+
 
 REDIS_URL = os.environ['REDIS_URL']
 REDIS_CHAN = 'chat'
@@ -132,4 +133,15 @@ def admin():
 @app.route('/login/')
 def login():
     return render_template('login.html')
+
+
+@app.route('/login',methods=['POST'])
+def do_admin_login():
+    if request.form['password'] =='password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return admin()
+    else:
+        flash('wrong password!')
+        return login()
+    
 
